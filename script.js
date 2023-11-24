feda84148966296be77cff6b3a621f016a4f18fc
wifon <kaziorigor0902@gmail.com>
2023-11-24 18:58:06 +0100

    Simple Todo-List

---
+++ script.js
@@ -0,0 +1,22 @@
+const todoList = ['siema', 'Umyc naczynia'];
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  todoListHTML += `<p>${todo}</p>`;
+}
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+  const inputElement = document.querySelector('input');
+  const name = inputElement.value;
+
+  todoList.push(name);
+  console.log(todoList);
+  
+  inputElement.value = '';
+})
